04780d9739abbc5cf5d3ff2a42ce30ad30734a05
Mohannad Shazli <mohannadshazli@gmail.com>
2026-06-02 19:29:40 +0300

    feat(setup): resolve Prisma setup issues and integrate Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common/enums/version-type.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Healthy App API')
+    .setDescription('The Healthy Application API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
